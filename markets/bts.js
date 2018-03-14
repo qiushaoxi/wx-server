4f9f5d810eb81c764bac5e447c13a2701cdc2c7e
qiushaoxi <qiushaoxi@163.com>
2018-03-14 10:32:25 +0800

    add hitbtc BTC

--- markets/bts.js
+++ markets/bts.js
@@ -20,6 +20,9 @@ const GDEX_ETH = "1.3.2598";
 const GDEX_BTC = "1.3.2241";
 const GDEX_BTM = "1.3.2790";
 const GDEX_NEO = "1.3.2919";
+const GDEX_GXS = "1.3.3445";
+const OPEN_BTC = "1.3.861";
+const OPEN_ETH = "1.3.850";
 const YOYOW = "1.3.1093";
 const YOYOW_PRECISION = 100000;
 const BTM_PRECISION = 1000000;
@@ -29,6 +32,7 @@ const CNY_PRECISION = 10000;
 const BTS_PRECISION = 100000;
 const BTC_PRECISION = 100000000;
 const NEO_PRECISION = 10000000;
+const GXS_PRECISION = 1000000;
 
 
 
@@ -102,7 +106,6 @@ const call = function (base, target, precision, symbol, market) {
 }
 
 call(CNY, WWW_EOS, EOS_PRECISION, "EOS", "WWW.EOS");
-call(CNY, OPEN_EOS, EOS_PRECISION, "EOS", "OPEN.EOS");
 call(CNY, BTS, BTS_PRECISION, "BTS", "inner");
 call(CNY, GDEX_EOS, EOS_PRECISION, "EOS", "GDEX.EOS");
 call(CNY, GDEX_ETH, ETH_PRECISION, "ETH", "GDEX.ETH");
@@ -110,3 +113,7 @@ call(CNY, GDEX_BTC, BTC_PRECISION, "BTC", "GDEX.BTC");
 call(CNY, GDEX_BTM, BTM_PRECISION, "BTM", "GDEX.BTM");
 call(CNY, GDEX_NEO, NEO_PRECISION, "NEO", "GDEX.NEO");
 call(CNY, YOYOW, YOYOW_PRECISION, "YOYO", "inner");
+call(CNY, GDEX_GXS, GXS_PRECISION, "GXS", "GDEX.GXS");
+call(CNY, OPEN_BTC, BTC_PRECISION, "BTC", "OPEN.BTC");
+call(CNY, OPEN_ETH, ETH_PRECISION, "ETH", "OPEN.ETH");
+call(CNY, OPEN_EOS, EOS_PRECISION, "EOS", "OPEN.EOS");
---
+++ markets/hitbtc.js
@@ -0,0 +1,69 @@
+const superagent = require('superagent');
+require('superagent-proxy')(superagent);
+const config = require("../config.json");
+const swap = require("../lib/pair.js").swap;
+const Pair = require("../lib/pair.js").Pair;
+const common = require('../tools/common');
+const logger = common.getLogger("hitbtc");
+const cache = require('../tools/cache');
+
+
+const interval = config.interval;
+const depthSize = config.depth;
+const url = "https://api.hitbtc.com/api/2/public/orderbook/";
+
+/**
+ * 计算特定深度均价
+ * @param {*} group 报价数组
+ * @param {*} depth 报价深度
+ * @param {*} position 头寸
+ */
+function averagePrice(group, depth, _position) {
+
+    let amount = 0;
+    let average = 0;
+    let total = 0;
+    for (let i = 0; i < depth && amount < _position; i++) {
+        //zb深度数组每一项包含数量和价格，0是价格，1是数量
+        amount += 1 * group[i].size;
+        total += 1 * group[i].price * group[i].size
+        average = total / amount;
+    }
+    return average;
+}
+
+//调用 rest api
+function call(base, quote) {
+    let position = config.position[quote];
+    //pair 里的base 和 quote 反了
+    let symbol = base + quote;
+    let pair = new Pair(quote, base, "hitbtc");
+    superagent.get(url + symbol)
+        //.proxy('http://127.0.0.1:1087')//本地测试代理
+        .query({
+            "limit": depthSize
+        })
+        .end(function (err, res) {
+            if (err) {
+                logger.error("http error :" + err);
+            } else if (res.statusCode != 200) {
+                logger.error("status code :" + res.statusCode);
+                return;
+            } else {
+                let depth = common.safelyParseJSON(res.text);
+                let middlePrice = (1 * depth.ask[0].price + 1 * depth.bid[0].price) / 2;
+                let tokenPosition = position / middlePrice;
+                let buyPrice = averagePrice(depth.ask, depthSize, tokenPosition);
+                let sellPrice = averagePrice(depth.bid, depthSize, tokenPosition);
+                pair.buyPrice = buyPrice;
+                pair.sellPrice = sellPrice;
+                cache.insertPair(pair);
+            }
+        });
+}
+
+//轮询获取最新价格
+setInterval(() => {
+    call("EOS", "ETH");
+    call("NEO", "ETH");
+}, interval);
